84d5b197eeb207d9d97f2e07b9fddf8792af8a8f
MoroHicham <moro.hicham.dev@gmail.com>
2023-02-28 03:15:01 +0100

    configure babel js and update template extension

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 
 // To import any lib in nodejs , we use required function 
-const express=require('express');
-const app=express();
+var express = require('express')
+    , cons = require('consolidate')
+    , app = express(),
+    bodyParser = require('body-parser')
 
+// assign the mustache engine to .html files
+app.engine('html', cons.mustache);
+
+// set .html as the default extension 
+app.set('view engine', 'html');
+
+app.set('views', __dirname + '/app/components');
+
+app.use(express.static(__dirname + '/app/styless'));
+
+app.use(bodyParser.json())
 // Server Creation 
 
 // Each server has an ip adress and a Port Number : in this case we have localhost : 127.0.0.1 with any chosen port
-const port= process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 // Endpoint Creation : URL : https://localhost:8000/index
 // HTTP Methods : GET ? POST ? PUT ? DELETE 
-app.get('/',(req,res)=> {
-    res.set('Content-Type', 'text/html');
-    res.send('<h1>Hello World</h1>');
+// test mustache
+app.get('/', function (req, res) {
+    var viewdata = { 'test': 'Hey now.' };
+    res.render('index', viewdata);
 });
 
 // Listener side creation 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log('Server app listening on port ' + port);
 });
